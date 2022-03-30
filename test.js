a6fb1bff0ee53cd50c06e0cb52bfa8e0638591f2
LucaNotaro95 <100934253+LucaNotaro95@users.noreply.github.com>
2022-03-30 17:41:41 +0200

    Update test.js
    file aggiornato

--- test.js
+++ test.js
@@ -1,29 +1,59 @@
 
+function sconto(a) {
+    let codiceScontoAccettato = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+    let sconto = document.getElementById("codiceSconto").value;
+    let prezzo = a;
 
+    for (i = 0; i < codiceScontoAccettato.length; i++) {
 
+        if (sconto == codiceScontoAccettato[i]) {
 
-function calcolo(){
-let BackEnd = 20.5;
+            alert("accettato codice sconto");
+            alert(prezzo);
+            prezzo = prezzo * 0.25;
+            alert(prezzo);
 
-let ore = document.getElementById("ore").value; 
-    ore = parseInt(ore)
-    alert(ore);
-let lavoro = document.getElementById("lavoro").value; 
-    alert(lavoro);
+            document.getElementById("risultato").innerHTML = "Il prezzo finale è di : " + prezzo + "\u20AC";
+        }
 
-let prezzofinito=0;
-if (lavoro==BackEnd){
+        else {
+            document.getElementById("risultato").innerHTML = "Il prezzo finale è di : " + prezzo + "\u20AC";
 
-prezzofin=BackEnd* ore;
+        }
+
+    }
 }
-  
+function calcolo() {
+
+    var sviluppoBackEnd = 20.5;
+    var sviluppoFrontEnd = 15.3;
+    var projectAnalist = 33.6;
+    var prezzo;
+
+    let ore = document.getElementById("ore").value;
+    ore = parseInt(ore);
+    let lavoro = document.getElementById("lavoro").value;
+    
+    if (lavoro == 1) {
+        prezzo = sviluppoBackEnd * ore;
+        sconto(prezzo);
+    }
+
+    if (lavoro == 2) {
+        prezzo = sviluppoFrontEnd * ore;
+        sconto(prezzo);
+
+    }
+
+    if (lavoro == 3) {
+        prezzo = projectAnalist;
+        sconto(prezzo);
+    }
+
 }
 
 
 
-document.getElementById("risultato").innerHTML = "Il prezzo finale è di : " + prezzofinito;
-
-}
 
 
 
@@ -31,4 +61,17 @@ document.getElementById("risultato").innerHTML = "Il prezzo finale è di : " + p
 
 
 
-   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
